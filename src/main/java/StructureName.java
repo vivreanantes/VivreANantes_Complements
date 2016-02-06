94f982030b177f1a5e1f530b22165e23bbc029e8
Renoulin Christian <chris-ren@netcourrier.com>
2016-02-06 10:51:42 +0100

    code java integre

---
+++ src/main/java/StructureName.java
@@ -0,0 +1,26 @@
+﻿
+public enum StructureName {
+	  //Objets directement construits
+	SAINT_ANDRE_DES_EAUX("SAINT-ANDRÉ-DES-EAUX"),
+	PAIMBOEUF("PAIMBOEUF"),
+	FROSSAY("FROSSAY"),
+	SAINT_PERE_EN_RETZ("SAINT PERE EN RETZ"),
+	DONGES("DONGES"),
+	SAINT_BREVIN_LES_PINS("SAINT-BRÉVIN LES PINS"),
+	SAINT_MALO_DE_GUERSAC("SAINT-MALO-DE-GUERSAC"),
+	SAINT_JOACHIM("SAINT-JOACHIM"),
+	PORNICHET("PORNICHET"),
+	CUNEIX("CUNEIX"),
+	MEAN_PENHOET_SAINT_NAZAIRE("MÉAN PENHOËT SAINT-NAZAIRE");
+	    
+	  private String name = "";
+	    
+	  //Constructeur
+	  StructureName(String name){
+	    this.name = name;
+	  }
+	    
+	  public String toString(){
+	    return name;
+	  }
+}
---
+++ src/main/java/ionic/MieuxTrierANantesV2Test.java
@@ -0,0 +1,16 @@
+package ionic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class MieuxTrierANantesV2Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
