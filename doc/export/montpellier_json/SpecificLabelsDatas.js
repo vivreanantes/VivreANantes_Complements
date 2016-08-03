6be3dd7c345f09d0881ff0df0a7ebc0f88fb3d77
Renoulin Christian <chris-ren@netcourrier.com>
2016-08-03 23:19:38 +0200

    bouton export sur les tableurs

---
+++ doc/export/montpellier_json/SpecificLabelsDatas.js
@@ -0,0 +1,6 @@
+var _specificLabelsDatas = [
+'titre' : {"fr":"Mieux trier à Montpellier",} ,
+'accueil1' : {"fr":"Bienvenue sur",} ,
+'accueil2' : {"fr":"Application d'aide au tri efficace sur la Métropole de Montpellier.",} ,
+'accueil3' : {"fr":"L'application de recyclage pour tous les âges !",} ,
+{} ];
